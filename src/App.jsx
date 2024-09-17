a1b15a7ea41694dd383ac61bb0047218af89c8ad
SHAKEEL AHMED <shakeelahmadprogrammer@gmail.com>
2024-09-17 01:29:19 -0700

    deployed website with only navigation bar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      </Routes> 
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
